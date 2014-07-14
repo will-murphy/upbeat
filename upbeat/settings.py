453c1405a266e86a0ed83e6812ea00ce898d8026
William Tennien Murphy <will.t.murphy@gmail.com>
2014-07-13 22:32:25 -0400

    debug

--- upbeat/settings.py
+++ upbeat/settings.py
@@ -61,8 +61,8 @@ WSGI_APPLICATION = 'upbeat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # dev
-        # 'ENGINE': 'django.db.backends.mysql', # prod
+        # 'ENGINE': 'django.db.backends.sqlite3', # dev
+        'ENGINE': 'django.db.backends.mysql', # prod
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
