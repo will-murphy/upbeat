81eb5bf5336a72e0dadd96e29525a0387389845c
Austin Chan <auscwork@gmail.com>
2014-07-20 15:41:13 -0400

    ok

--- static/assets/script.js
+++ static/assets/script.js
@@ -144,9 +144,9 @@ function htmlEscape(str) {
             .replace(/>/g, '&gt;');
 }
 function stuffEncode(text){
-    var result = htmlEscape(text);
-    result = linkify(text);
-    result = result.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    var result = linkify(text);
+    result = htmlEscape(result);
+    result = result.replace(/(?:\r\n|\r|\n)/g, '<br>');
     return result;
 }
 function linkFilter(url){
